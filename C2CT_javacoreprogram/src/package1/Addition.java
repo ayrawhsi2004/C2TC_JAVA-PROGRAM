611413e5a776f13fd9be7b9de33aea6a5d64a02d
ayrawhsi2004 <22eee009@act.edu.in>
2025-06-13 10:48:16 +0530

    this is my addition program

---
+++ C2CT_javacoreprogram/src/package1/Addition.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Addition {
+ public static void main (String args[]) {
+	int a,b,c;
+	  a=Integer.parseInt(args[0]);
+	  b=Integer.parseInt(args[1]);
+	  c=a+b;
+	  System.out.println("FirstProgram of a="+a+"and b="+b+" is "+c);
+
+
+	}
+
+}
