296bd5c217e1331cff984b23eb15c6a4a2783efa
ayrawhsi2004 <22eee009@act.edu.in>
2025-06-16 15:03:30 +0530

    this is getter and setter program

---
+++ C2CT_javacoreprogram/src/Demo/Student.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Student {
+public static void main(String args[]) {
+	StudentDemo st = new StudentDemo();
+	st.setId(101);
+	st.setName("jon");
+	st.setAge(20);
+	
+
+System.out.println("Id:"+st.getId());
+System.out.println("Name:"+st.getName());
+System.out.println("Age:"+st.getAge());
+}}
